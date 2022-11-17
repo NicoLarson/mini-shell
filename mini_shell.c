b0a279375a6a00c05689022f12472d893f1110b0
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-11-17 10:04:04 -0300

    Update execv

--- mini_shell.c
+++ mini_shell.c
@@ -6,33 +6,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define NUM_OF_COMMAND 7
-
-char *my_commands[NUM_OF_COMMAND] = {"mls", "mpwd", "mcd", "mfind", "mcat", "mgrep", "mhist"};
-
-char *command_detector(char *first_word)
-{
-	char *command_detected;
-	for (int i = 0; i < NUM_OF_COMMAND; i++)
-	{
-		if ((strstr(first_word, my_commands[i])) == NULL)
-		{
-			command_detected = 0;
-		}
-		else
-		{
-			command_detected = my_commands[i];
-			i = NUM_OF_COMMAND;
-		}
-	}
-
-	return command_detected;
-}
-
-void parameter_detector(char *parameter)
-{
-}
-
 void add_to_history_file(char *to_add)
 {
 	FILE *history_file;
@@ -81,33 +54,19 @@ int mcd(char *directory)
 	return 0;
 }
 
-pid_t spawnChild(char *program, char **arg_list)
+// Function who concatenate 2 strings
+char *concatenate(char *s1, char *s2)
 {
-	pid_t ch_pid = fork();
-	if (ch_pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-
-	if (ch_pid > 0)
-	{
-		printf("spawn child with pid - %d\n", ch_pid);
-		return ch_pid;
-	}
-	else
-	{
-		execvp(program, arg_list);
-		perror("execve");
-		exit(EXIT_FAILURE);
-	}
+	char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+	// in real code you would check for errors in malloc here
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
 }
-
 int mini_shell(void)
 {
-	// malloc
 	char *leaving_prog_directory = "/mnt/c/Users/nyang2/OneDrive - Université de Guyane/Documents/COURS/SYSTEM/mini-shell/build/";
-	// strcat : copier la chaine
+	char **arg_list = malloc(100 * sizeof(char *));
 	int exit_mini_shell = 0;
 	while (exit_mini_shell == 0)
 	{
@@ -118,19 +77,31 @@ int mini_shell(void)
 		nread = getline(&input_string, &n, stdin);
 		// Enregistrer dans une variable la commande
 		char *first_word = strtok(input_string, " ");
-		printf("%s", strcat(prog_directory, first_word));
-		// Enregistrer dans un tableau dynamique les arguments
-		// Créer un processus fils et executer les programmes
-		char *args[] = {first_word, NULL};
-		pid_t child;
-		int wstatus;
-		child = spawnChild("aze", args);
-
-		if (waitpid(child, &wstatus, WUNTRACED | WCONTINUED) == -1)
+		char *command = concatenate(leaving_prog_directory, first_word);
+		// Enregistrer les arguments dans le tableau arguments
+		int i = 0;
+		while (first_word != NULL)
 		{
-			perror("waitpid");
-			exit(EXIT_FAILURE);
+			arg_list[i] = first_word;
+			first_word = strtok(NULL, " ");
+			i++;
 		}
+		arg_list[i] = NULL;
+		// afficher les tableau
+		for (int j = 0; j < i; j++)
+		{
+			printf("%s\n", arg_list[j]);
+		}
+
+		// Detecter la commande
+		// executer les programmes
+		// TODO : gerer les argument
+
+		printf("command - %s\n", command);
+		char *args[] = {first_word, NULL};
+		// execvp fonctionne avec des arguments
+		execvp(command, args);
+		perror("execve");
 
 		if (nread == -1)
 		{
@@ -145,7 +116,6 @@ int mini_shell(void)
 			exit_mini_shell = 1;
 		}
 		/* Libérer la mémoire */
-		printf("free input string");
 		perror("execvp");
 
 		free(input_string);
