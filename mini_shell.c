b337e50442bae45506fd8fd7282e742057782f88
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-11-17 15:54:33 -0300

    update mini_shell.c
    Problème de lecture des argument avec exec()

--- mini_shell.c
+++ mini_shell.c
@@ -23,23 +23,6 @@ void add_to_history_file(char *to_add)
 	fclose(history_file);
 }
 
-void run_command(char *command_name, char *arguments)
-{
-	char command_to_run[] = "./build/";
-	strcat(command_to_run, command_name);
-	strcat(command_to_run, " ");
-
-	if (arguments != NULL)
-	{
-		strcat(command_to_run, arguments);
-		system(command_to_run);
-	}
-	else
-	{
-		system("./build/mcat");
-	}
-}
-
 int mcd(char *directory)
 {
 	int ch = chdir(directory);
@@ -54,73 +37,58 @@ int mcd(char *directory)
 	return 0;
 }
 
-// Function who concatenate 2 strings
 char *concatenate(char *s1, char *s2)
 {
-	char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-	// in real code you would check for errors in malloc here
+	char *result = malloc(strlen(s1) + strlen(s2));
 	strcpy(result, s1);
 	strcat(result, s2);
 	return result;
 }
 int mini_shell(void)
 {
-	char *leaving_prog_directory = "/mnt/c/Users/nyang2/OneDrive - Université de Guyane/Documents/COURS/SYSTEM/mini-shell/build/";
+	char *leaving_prog_directory = "./build/";
 	char **arg_list = malloc(100 * sizeof(char *));
+	char *input = malloc(100 * sizeof(char));
+
 	int exit_mini_shell = 0;
 	while (exit_mini_shell == 0)
 	{
-		size_t n = 0;
-		char *input_string = NULL;
-		ssize_t nread;
-		printf("[MINI-SHELL]-$: ");
-		nread = getline(&input_string, &n, stdin);
-		// Enregistrer dans une variable la commande
-		char *first_word = strtok(input_string, " ");
-		char *command = concatenate(leaving_prog_directory, first_word);
-		// Enregistrer les arguments dans le tableau arguments
+		printf("mini-shell> ");
+		fgets(input, 100, stdin);
+		// insert input word in arg_list
+		char *token = strtok(input, " ");
 		int i = 0;
-		while (first_word != NULL)
+		while (token != NULL)
 		{
-			arg_list[i] = first_word;
-			first_word = strtok(NULL, " ");
+			arg_list[i] = token;
+			token = strtok(NULL, " ");
 			i++;
 		}
 		arg_list[i] = NULL;
-		// afficher les tableau
-		for (int j = 0; j < i; j++)
-		{
-			printf("%s\n", arg_list[j]);
-		}
-
-		// Detecter la commande
-		// executer les programmes
-		// TODO : gerer les argument
-
-		printf("command - %s\n", command);
-		char *args[] = {first_word, NULL};
-		// execvp fonctionne avec des arguments
-		execvp(command, args);
-		perror("execve");
-
-		if (nread == -1)
-		{
-			printf("nread == -1 ");
-
-			perror("getline");
-			return EXIT_FAILURE;
-		}
-
-		if (strcmp(input_string, "exit") == 10)
+		// if input is exit, exit the program
+		if (strcmp(arg_list[0], "exit") == 0)
 		{
 			exit_mini_shell = 1;
 		}
-		/* Libérer la mémoire */
-		perror("execvp");
-
-		free(input_string);
+		else
+		{
+			char *command_build = concatenate(leaving_prog_directory, arg_list[0]);
+			char *command = strtok(command_build, "\n");
+			pid_t child_pid;
+			child_pid = fork();
+			if (child_pid != 0)
+			{
+				int child_status;
+				waitpid(child_pid, &child_status, 0);
+			}
+			else
+			{
+				execvp(command, arg_list);
+			}
+		}
 	}
+	free(arg_list);
+	free(input);
 	printf("EXIT");
-
 	return EXIT_SUCCESS;
 }
